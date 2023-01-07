77802e062cb8aab09cfb20e5b68854e2eff5a06c
inthej <inthej12@gmail.com>
2023-01-07 15:25:25 +0900

    프로그래머스: 숫자 짝꿍

---
+++ src/programmers/level1/숫자_짝꿍.js
@@ -0,0 +1,81 @@
+/*
+숫자 짝꿍
+문제 설명
+두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다(단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다). X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
+
+예를 들어, X = 3403이고 Y = 13203이라면, X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다. 다른 예시로 X = 5525이고 Y = 1255이면 X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다(X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
+
+제한사항
+3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
+X, Y는 0으로 시작하지 않습니다.
+X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
+입출력 예
+X	Y	result
+"100"	"2345"	"-1"
+"100"	"203045"	"0"
+"100"	"123450"	"10"
+"12321"	"42531"	"321"
+"5525"	"1255"	"552"
+입출력 예 설명
+입출력 예 #1
+
+X, Y의 짝꿍은 존재하지 않습니다. 따라서 "-1"을 return합니다.
+입출력 예 #2
+
+X, Y의 공통된 숫자는 0으로만 구성되어 있기 때문에, 두 수의 짝꿍은 정수 0입니다. 따라서 "0"을 return합니다.
+입출력 예 #3
+
+X, Y의 짝꿍은 10이므로, "10"을 return합니다.
+입출력 예 #4
+
+X, Y의 짝꿍은 321입니다. 따라서 "321"을 return합니다.
+입출력 예 #5
+
+지문에 설명된 예시와 같습니다.
+ */
+
+function solution(X, Y) {
+  const twinValues = ValueUtils.twinValues(X, Y)
+  const result = twinValues.sort((a, b) => b - a).join('')
+  if (ValueUtils.empty(result)) {
+    return '-1'
+  }
+
+  if (result[0] === '0') {
+    return '0'
+  }
+
+  return result
+}
+
+const ValueUtils = {}
+ValueUtils.empty = (str, includeBlank = true) => {
+  const empty = str === undefined || str === null || (includeBlank && str === '')
+  return empty
+}
+
+ValueUtils.twinValues = (str1, str2) => {
+  const result = []
+  const countMap = new Map()
+  for (const ch of str1) {
+    let count = countMap.get(ch)
+    count = (count || 0) + 1
+    countMap.set(ch, count)
+  }
+
+  for (const ch of str2) {
+    const count = countMap.get(ch)
+    if (count > 0) {
+      result.push(ch)
+      countMap.set(ch, count - 1)
+    }
+  }
+  return result
+}
+
+console.log(solution('100', '2345'))
+console.log(solution('100', '203045'))
+console.log(solution('100', '123450'))
+console.log(solution('12321', '42531'))
+console.log(solution('5525', '1255'))
